079752d9869d4afc99da016ad366116a7c49e5f2
Julian Sommer <115516918+juliansommer@users.noreply.github.com>
2025-04-28 15:31:29 +1000

    nicer output

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ def main() -> None:
     # hardcoding limit of 1000 to get all images, but if images are more than 1000, it will return 1000 images
     # this 100 limit is enforced by the Imgur api, so we need to use the limit parameter to get all images
     images_objects = user.get_images(limit=1000)
-    print(f"Found {len(images_objects)} images")
 
     image_links = []
 
@@ -29,14 +28,18 @@ def main() -> None:
     # find all links in md files
     found_links = find_matches_in_files(path_input, regex, ".md")
 
-    print(f"Found {len(found_links)} links in md files")
-    print(f"Found {len(image_links)} links in imgur api")
+    print(f"Found {len(set(found_links))} links in files")
+    print(f"Found {len(image_links)} links in Imgur api")
 
     # Output Imgur api links that are not in md files
     missing_links = set(image_links) - set(found_links)
-    print("Imgur links not found in any md file:")
-    for link in missing_links:
-        print(link)
+
+    if not missing_links:
+        print("All Imgur links are found in files")
+    else:
+        print(f"Found {len(missing_links)} Imgur links not found in files")
+        for link in missing_links:
+            print(link)
 
 
 if __name__ == "__main__":
